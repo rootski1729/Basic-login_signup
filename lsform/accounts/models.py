97b789a8f1fa29621e6f9d03dbc12bb62aa6556e
rahuljoshi1729 <rahul22100@akgec.ac.in>
2025-01-13 16:33:33 +0530

    updated custom model.py

--- lsform/accounts/models.py
+++ lsform/accounts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+type_user = (
+    ('patient', 'Patient'),
+    ('doctor', 'Doctor'),
+)
+class CustomUser(AbstractUser):
+    user_type = models.CharField(choices=type_user, max_length=10)
+    profile_pic = models.ImageField(upload_to='profile_pic/', null=True, blank=True)
+    address_line1 = models.CharField(max_length=255, null=True, blank=True)
+    city = models.CharField(max_length=100, null=True, blank=True)
+    state = models.CharField(max_length=255, null= True, blank = True)
+    pincode = models.CharField(max_length=10,blank=True, null=True)
+    
+    def __str__(self):
+        return self.username
+    
