8d8a84c1ab5eef1171a629fa4e607d92fb214338
rahuljoshi1729 <rahul22100@akgec.ac.in>
2025-01-26 00:05:33 +0530

    updated database

--- lsform/lsform/settings.py
+++ lsform/lsform/settings.py
@@ -14,7 +14,9 @@ from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -83,10 +85,14 @@ WSGI_APPLICATION = 'lsform.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default': {'ENGINE': 'django.db.backends.postgresql',
+    'NAME': os.getenv('db_name'),
+    'USER':os.getenv('db_user'),
+    'PASSWORD':os.getenv('db_password'),
+    'HOST':os.getenv('db_host'),
+    'PORT':os.getenv('db_port')
     }
+
 }
 
 
