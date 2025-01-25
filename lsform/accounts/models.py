491d9b6cf41dba7438072493a1e5807f2ecc7e40
rahuljoshi1729 <rahul22100@akgec.ac.in>
2025-01-25 22:28:51 +0530

    update models.py for blog

--- lsform/accounts/models.py
+++ lsform/accounts/models.py
@@ -16,3 +16,30 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.username
     
+blogcategory = [('covid19','covid19'),('Mental health','Mental health'),('fitness','fitness'),('food','food'),('lifestyle','lifestyle')]
+class Blogcategory(models.Model):
+    name = models.CharField(max_length=255,unique=True,choices=blogcategory)
+    
+    def __str__(self):
+        return self.name
+    
+class Blogpost(models.Model):
+    DRAFT = 'draft'
+    PUBLISHED= 'published'
+    STATUS = [
+        (DRAFT, 'draft'),
+        (PUBLISHED, 'published'),
+    ]
+    
+    title = models.CharField(max_length=255)
+    image= models.ImageField(upload_to='blog_image/',blank=True,null=True)
+    category = models.ForeignKey(Blogcategory, on_delete=models.CASCADE,related_name='category')
+    summary = models.TextField(max_length=300)
+    content= models.TextField()
+    status= models.CharField(max_length=10,choices=STATUS,default=DRAFT)
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE,related_name='author')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
