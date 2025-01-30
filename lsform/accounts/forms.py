f3d3d2ccef9dc9cb5841f7cc08588d851ed83f1a
rahuljoshi1729 <rahul22100@akgec.ac.in>
2025-01-30 19:15:16 +0530

    updated to mysql

--- lsform/accounts/forms.py
+++ lsform/accounts/forms.py
@@ -4,5 +4,13 @@ from .models import Blogpost
 class Blogpostform(forms.ModelForm):
     class Meta:
         model = Blogpost
-        fields = ['title', 'image', 'category', 'summary', 'content', 'status']
+        fields = ['title', 'content','summary' ,'image', 'category', 'status']  # Include 'status' field
+
+    def __init__(self, *args, **kwargs):
+        super(Blogpostform, self).__init__(*args, **kwargs)
+        self.fields['status'].widget = forms.RadioSelect(choices=[
+            ('published', 'Published'),
+            ('drafted', 'Drafted')
+        ])
+        self.fields['status'].initial = 'published'
         
